fbc529d4dde77ea2c712612c5df261498bd7cd4f
Mischa S <revmischa@cpan.org>
2007-07-09 02:42:21 +0000

    update to use NetConsole correctly

--- tools/netvm.pl
+++ tools/netvm.pl
@@ -27,7 +27,7 @@ my %handlers = (
 my $msg;
 
 if ($infile) {
-    load($infile);
+    load(undef, $infile);
     $msg = dis_current_instruction();
 }
 
@@ -39,6 +39,108 @@ my $con = NetConsole->new(
                           );
 
 $con->run;
+
+
+########### commands
+
+sub load {
+    my ($self, $filename) = @_;
+
+    my $contents = slurp($filename) or return undef;
+    my $bytecode = pack("C*", unpack("C*", $contents));
+
+    return vmload($bytecode);
+}
+
+sub asld {
+    my ($self, $filename) = @_;
+    my $code = slurp($filename);
+    my $bytecode = NetCode->assemble($code);
+    return vmload($bytecode);
+}
+sub regs {
+    my @args = @_;
+
+    my $regs = $vm->regs;
+
+    my $ret = '';
+    my $i = 0;
+
+    $ret .= sprintf "PC: 0x%08X\n", $vm->pc;
+
+    foreach my $reg (@$regs) {
+        my $regname = $vm->reg_name($i);
+        $ret .= sprintf "[%02d %4s]: 0x%08X   ", $i++, $regname, $reg;
+        $ret .= "\n" if $i % 2 == 0;
+    }
+
+    return $ret;
+}
+
+sub reset {
+    my $self = shift;
+    $vm->init;
+    return $self->load($infile);
+}
+
+sub run {
+    my ($self) = @_;
+
+    print "Running...\n";
+    while ($vm->step) {
+
+    }
+
+    return "Program reached end of memory";
+}
+
+sub step {
+    my ($self) = @_;
+
+    if ($vm->step) {
+        return dis_current_instruction();
+    } else {
+        return "End of program reached";
+    }
+}
+
+# returns disassembly of current instruction
+sub dis_current_instruction {
+    return sprintf "0x%08X> %s\n", $vm->pc, NetCode->disassemble_string($vm->current_instruction);
+}
+
+sub mem {
+    my ($self) = @_;
+
+    my $ret = '';
+    my $bytes;
+
+    my $size = $vm->mem->size;
+    $size = $size > 128 ? 128 : $size;
+    for (my $pos = 0; $pos < $size; $pos++) {
+        my $byte = unpack("C", $vm->mem->get($pos, 1));
+        $ret .= sprintf "%08X: 0x%02X %3d 0b%08b %c\n", $pos, $byte, $byte, $byte, $byte;
+    }
+
+    return $ret;
+}
+
+sub help {
+    my ($self) = @_;
+
+    return q {
+load (filename) - loads a file containing NetCode
+asld (filename) - (assemble and load) assemble a NetAsm file and load it
+mem - dump memory
+step - step program one instruction
+run - runs program
+regs - dump registers
+quit - quit
+};
+}
+
+###########
+
 
 # slurp in a file and return its contents, returns undef on error
 sub slurp {
@@ -66,21 +168,6 @@ sub slurp {
     return $contents;
 }
 
-sub load {
-    my ($filename) = @_;
-
-    my $contents = slurp($filename) or return undef;
-    my $bytecode = pack("C*", unpack("C*", $contents));
-
-    return vmload($bytecode);
-}
-
-sub asld {
-    my $code = slurp(shift());
-    my $bytecode = NetCode->assemble($code);
-    return vmload($bytecode);
-}
-
 sub vmload {
     my $bytecode = shift;
 
@@ -91,82 +178,3 @@ sub vmload {
     return dis_current_instruction();
 }
 
-sub regs {
-    my @args = @_;
-
-    my $regs = $vm->regs;
-
-    my $ret = '';
-    my $i = 0;
-
-    $ret .= sprintf "PC: 0x%08X\n", $vm->pc;
-
-    foreach my $reg (@$regs) {
-        my $regname = $vm->reg_name($i);
-        $ret .= sprintf "[%02d %4s]: 0x%08X   ", $i++, $regname, $reg;
-        $ret .= "\n" if $i % 2 == 0;
-    }
-
-    return $ret;
-}
-
-sub reset {
-    $vm->init;
-    return load($infile);
-}
-
-sub run {
-    my @args = @_;
-
-    print "Running...\n";
-    while ($vm->step) {
-
-    }
-
-    return "Program reached end of memory";
-}
-
-sub step {
-    my @args = @_;
-
-    if ($vm->step) {
-        return dis_current_instruction();
-    } else {
-        return "End of program reached";
-    }
-}
-
-# returns disassembly of current instruction
-sub dis_current_instruction {
-    return sprintf "0x%08X> %s\n", $vm->pc, NetCode->disassemble_string($vm->current_instruction);
-}
-
-sub mem {
-    my @args = @_;
-
-    my $ret = '';
-    my $bytes;
-
-    my $size = $vm->mem->size;
-    $size = $size > 128 ? 128 : $size;
-    for (my $pos = 0; $pos < $size; $pos++) {
-        my $byte = unpack("C", $vm->mem->get($pos, 1));
-        $ret .= sprintf "%08X: 0x%02X %3d 0b%08b %c\n", $pos, $byte, $byte, $byte, $byte;
-    }
-
-    return $ret;
-}
-
-sub help {
-    my @args = @_;
-
-    return q {
-load (filename) - loads a file containing NetCode
-asld (filename) - (assemble and load) assemble a NetAsm file and load it
-mem - dump memory
-step - step program one instruction
-run - runs program
-regs - dump registers
-quit - quit
-};
-}
