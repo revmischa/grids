af7173e2d25e529cdfbc017e50d60f4018d1c238
Mischa S <revmischa@cpan.org>
2007-09-03 00:28:50 +0000

    more hook functionality

--- lib/perl/nethooks.pl
+++ lib/perl/nethooks.pl
@@ -1,20 +1,40 @@
-sub run_event_hooks {
-    my ($self, %info) = @_;
+# run hooks and require them all to return true
+sub test_all_hooks {
+    my ($self, $hookname, %info) = @_;
 
-    return undef unless $self->{hooks};
+    return ! grep { ! $_ } $self->run_hooks($hookname, %info);
+}
 
-    my $event = $info{event};
-    my $hooks = $self->{hooks}->{"Event-$event"};
-    return undef unless $hooks && @$hooks;
+# run hooks and require at least one to return true
+sub test_any_hook {
+    my ($self, $hookname, %info) = @_;
+
+    return grep { $_ } $self->run_hooks($hookname, %info);
+}
+
+sub run_hooks {
+    my ($self, $hookname, %info) = @_;
+
+    my $hooks = $self->{hooks}->{$hookname};
+    return () unless $hooks && @$hooks;
 
     my @res;
 
     foreach my $hook (@$hooks) {
         my $ret = $hook->($self, %info);
-        push @res, $ret if $ret;
+        push @res, $ret;
     }
 
-    return @res || undef;
+    return @res;
+}
+
+sub run_event_hooks {
+    my ($self, %info) = @_;
+
+    my $event = $info{event};
+    my $hookname = "Event-$event";
+
+    return $self->run_hooks($hookname, %info);
 }
 
 sub register_event_hook {
