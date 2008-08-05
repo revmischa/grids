ea272801394ccebadb9460fcf81c3d1cdcd75fa8
Patrick T <patrick.l.tierney@gmail.com>
2008-08-05 20:41:00 +0000

    xcode next

---
+++ cpp/irrlicht/xcode/grids_view_01/GridsDefine.h
@@ -0,0 +1,16 @@
+
+#ifndef GRIDS_DEFINE_INCLUDE
+#define GRIDS_DEFINE_INCLUDE
+
+#include <string>
+#include <map>
+
+namespace Grids
+{
+
+	typedef  std::map< std::string, std::string > gridsmap_t;
+
+}
+
+#endif
+
---
+++ cpp/irrlicht/xcode/grids_view_01/GridsHashList.h
@@ -0,0 +1,29 @@
+
+
+#ifndef GRIDS_HASH_LIST_INCLUDE
+#define GRIDS_HASH_LIST_INCLUDE
+
+
+// Stor
+
+class GridsHashList
+{
+    public:
+        addEvent( int eventType );
+
+
+    private:
+
+
+
+
+};
+
+
+
+
+
+
+
+
+# endif
