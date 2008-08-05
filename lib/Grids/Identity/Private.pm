5ad119e15145d3ff5b973a39071374ebc264cdaa
Mischa S <revmischa@cpan.org>
2008-08-05 02:31:42 +0000

    Grids::Identity helper modules

---
+++ lib/Grids/Identity/Private.pm
@@ -0,0 +1,26 @@
+package Grids::Identity::Private;
+
+use strict;
+use warnings;
+use base qw/Crypt::RSA::Key::Private::SSH/;
+
+sub new {
+    my $class = shift;
+    return $class->SUPER::new(KF => 'SSH', @_);
+}
+
+sub serialize {
+    my $self = shift;
+
+    my $serialized = $self->SUPER::serialize;
+    return $serialized;
+}
+
+sub deserialize {
+    my $class = shift;
+
+    my $self = $class->new;
+    return $self->SUPER::deserialize(@_);
+}
+
+1;
