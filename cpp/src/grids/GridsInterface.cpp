11c7c3c7157455720f214e9340e0ff35cd21e01c
Patrick T <patrick.l.tierney@gmail.com>
2008-08-07 21:45:52 +0000

    GridsMesenger, GridsPerson

---
+++ cpp/src/grids/GridsInterface.cpp
@@ -0,0 +1,65 @@
+/*
+ *  GridsInterface.cpp
+ *  grids_view_01
+ *
+ *  Created by Patrick Tierney on 8/7/08.
+ *  Copyright 2008 Patrick Tierney. All rights reserved.
+ *
+ */
+
+#include "GridsInterface.h"
+
+
+namespace Grids
+{
+	
+	GInterface::GInterface( const char * address, GObjectController * o_c_in, GPersonController * p_c_in, GMessengerController * m_c_in )
+		: node_address( address), object_controller( o_c_in ), person_controller( p_c_in ), messenger_controller( m_c_in )
+	{
+		object_controller->interface = &this;
+		person_controller->interface = &this;
+		messenger_controller->interface = &this;
+		
+		protocol = new Protocol();
+		
+		while( !protocol->connectToNode( node_address ) )
+		{
+			// This should probably be threaded in the future
+		}
+		
+		protocol->setEventCallback( receiveEvent );
+	}
+	
+	GInterface::sendEvent( GEvent * evt )
+	{
+		sendRequest( evt->getEventType(), evt->getMap() );
+	}
+	
+	GInterface::receiveEvent( Protocol * proto, GEvent * evt )
+	{
+		parseEventType( evt );
+	}
+	
+	GInterface::parseEventType(  GEvent * evt )
+	// This will be modified, person objects may need object information...
+	// mayme all items need all information??
+	{
+		std::string event_type = evt->getEventType();
+		
+		if( event_type == "PERSON" )
+		{
+			person_controller->giveEvent( evt );
+		}
+		else if( event_type == "OBJECT" )
+		{
+			object_controller->giveEvent( evt );
+		}
+		else if( event_type == "MESSENGER" )
+		{
+			messenger_controller->giveEvent( evt );
+		}
+	}
+		
+
+
+} // end namespace Grids
