7e37931510ced74f87a1e89cb197657b7d12516a
Mischa S <revmischa@cpan.org>
2008-08-07 20:01:45 +0000

    restore gridsdefine, update building to use src/grids

--- cpp/src/grids/GridsDefine.h
+++ cpp/src/grids/GridsDefine.h
@@ -8,10 +8,14 @@
 namespace Grids
 {
 
-	typedef  std::map< std::string, std::string > gridsmap_t;
-	
-	typedef gridsmap_t::iterator giterator;
 
+  /*  typedef const char * gridskey_t;
+      typedef const char * gridsval_t;*/
+
+  typedef std::string gridskey_t;
+  typedef std::string gridsval_t;
+  typedef std::map<gridskey_t, gridsval_t> gridsmap_t;
+  typedef gridsmap_t::iterator giterator;
 }
 
 
--- cpp/src/test.sh
+++ cpp/src/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-SRCS=../irrlicht/xcode/grids_view_01
+SRCS=grids/
 
-g++ GridsProtocol.cpp gridstest.cpp  $SRCS/GridsEvent.cpp  -o gridstest -I $SRCS -lpthread -I ../include ../external/jsoncpp/libs/linux-gcc-4.1.2/libjson_linux-gcc-4.1.2_libmt.a  && ./gridstest localhost
+g++ grids/GridsProtocol.cpp gridstest.cpp  $SRCS/GridsEvent.cpp  -o gridstest -I $SRCS -lpthread -I ../include ../external/jsoncpp/libs/linux-gcc-4.1.2/libjson_linux-gcc-4.1.2_libmt.a  && ./gridstest localhost
