f2fd8dc12b735ea8a8fa20209d0e470447595ecd
Mischa S <revmischa@cpan.org>
2008-08-10 09:06:04 +0000

    hooks for simple Room server

---
+++ lib/Grids/Node/Hooks/Room.pm
@@ -0,0 +1,32 @@
+package Grids::Node::Hooks::Room;
+
+use strict;
+use warnings;
+use Grids::Node;
+use Grids::Room;
+use Data::UUID;
+
+my $prefix = 'Room';
+
+Grids::Node->register_hooks(
+                            "$prefix.Create" => \&create,
+                            "$prefix.List" => \&list_rooms,
+                            );
+
+our %ROOMS;
+
+sub create {
+    my ($node, $args) = @_;
+
+    my $room = Grids::Room->create;
+
+    return $node->event_hook_success(id => $room->id);
+}
+
+sub list_rooms {
+    my ($node, $args) = @_;
+
+    return $node->event_hook_success(rooms => [ keys %ROOMS ]);
+}
+
+1;
