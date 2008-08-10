b94224cdf067b18c41b47ddc2ca996c529a3be06
Mischa S <revmischa@cpan.org>
2008-08-10 08:58:11 +0000

    use lib in svn for test

--- cpp/src/test.sh
+++ cpp/src/test.sh
@@ -2,4 +2,4 @@
 
 SRCS=grids
 
-g++ $SRCS/protocol.cpp gridstest.cpp  $SRCS/event.cpp  -o gridstest -I $SRCS -lpthread -I ../include ../external/jsoncpp/libs/linux-gcc-4.1.2/libjson_linux-gcc-4.1.2_libmt.a  && ./gridstest localhost
+g++ $SRCS/protocol.cpp gridstest.cpp  $SRCS/event.cpp  -o gridstest -I $SRCS -lpthread -I ../include ../lib/libjson_linux-gcc-4.1.2_libmt.a  && ./gridstest localhost
