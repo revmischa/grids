5004fbe144c9e3334a732c08fcb7b48716ae3893
Mischa S <revmischa@cpan.org>
2008-08-13 03:50:41 +0000

    add script to compile everything on linux

---
+++ cpp/src/test_full_linux.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+SRCS=grids
+
+g++ $SRCS/*.cpp  -o grids_full -I $SRCS -lSDL -lSDL_net -lGL -L../lib kaleidoscope/* -lglfw -I /usr/include/SDL -I ../include ../lib/libjson_linux-gcc-4.1.2_libmt.a  && ./gridstest localhost
