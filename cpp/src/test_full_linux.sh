27d3392cb3d8ed9bf3ea6dc3cfc1ec1250667fce
Mischa S <revmischa@cpan.org>
2008-08-14 00:28:31 +0000

    fix build

--- cpp/src/test_full_linux.sh
+++ cpp/src/test_full_linux.sh
@@ -2,4 +2,4 @@
 
 SRCS=grids
 
-g++ $SRCS/*.cpp  -o grids_full -I $SRCS -lSDL -lSDL_net -lGL -L../lib kaleidoscope/* -lglfw -I /usr/include/SDL -I ../include ../lib/libjson_linux-gcc-4.1.2_libmt.a  && ./grids_full
+g++ $SRCS/*.cpp  -o grids_full -I $SRCS -lSDL -lSDL_net -lGL -L../lib kaleidoscope/*  -I ../include ../lib/libjson_linux-gcc-4.1.2_libmt.a  && ./grids_full
