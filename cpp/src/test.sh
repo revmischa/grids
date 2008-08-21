72ebdfbdc43ba11d8342d595f52c3042f3a493a0
Mischa S <revmischa@cpan.org>
2008-08-21 05:00:03 +0000

    generate debug symbols in cli test client

--- cpp/src/test.sh
+++ cpp/src/test.sh
@@ -2,4 +2,4 @@
 
 SRCS=grids
 
-g++ -lm $SRCS/protocol.cpp gridstest.cpp  $SRCS/event.cpp  -o gridstest -I $SRCS -lSDL -lSDL_net -lpthread -I ../include ../lib/libjson_linux-gcc-4.1.2_libmt.a  && ./gridstest localhost
+g++ -g -lm $SRCS/protocol.cpp gridstest.cpp  $SRCS/event.cpp  -o gridstest -I $SRCS -lSDL -lSDL_net -lpthread -I ../include ../lib/libjson_linux-gcc-4.1.2_libmt.a  && ./gridstest localhost
