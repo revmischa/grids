a8d9b29a5cdbc94226ea68278030e03a56f5a5f1
Mischa S <revmischa@cpan.org>
2010-01-18 02:04:11 +0000

    POD docs for Grids

---
+++ lib/Grids.pm
@@ -0,0 +1,51 @@
+package Grids;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+
+
+__END__
+
+=head1 NAME
+
+Grids - Reference implementation of the Grids application platform and
+protocol
+
+=head1 SYNOPSIS
+
+    perl Makefile.PL
+    make
+
+=head1 DESCRIPTION
+
+The Grids development platform aims to provide a common foundation for
+network-based applications. It is designed to be open, cross-platform
+and powerful, providing a standard set of protocols defining modern
+components of any net-aware application, including cryptography,
+redundant client/server architecture, data storage and distributed
+communication.
+
+=head2 EXPORT
+
+No namespace pollution tolerated.
+
+=head1 AUTHORS
+
+    Mischa Spiegelmock, E<lt>mspiegelmock@gmail.comE<gt>
+
+    Patrick Tierney, E<lt>patrick.l.tierney@gmail.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2010 by Mischa Spiegelmock
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.8 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
