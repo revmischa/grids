abf7a13c44008567b9a3f65f646a826d29cc8f23
Mischa S <revmischa@cpan.org>
2010-02-21 04:15:49 +0000

    null transport

---
+++ lib/Grids/Transport/Null.pm
@@ -0,0 +1,17 @@
+# Grids::Transport equiviliant of /dev/null
+
+package Grids::Transport::Null;
+
+use Moose;
+with 'Grids::Transport::Driver';
+
+has delegate => (
+    is => 'rw',
+);
+
+sub write {}
+sub connect {}
+sub disconnect {}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
